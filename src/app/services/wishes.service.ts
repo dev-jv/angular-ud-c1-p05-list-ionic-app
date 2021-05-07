d0e408306a91ee2cb6a6783a374349560a404261
dev-jv <dev.javierleon@gmail.com>
2021-05-07 17:15:50 -0500

    eight

--- src/app/services/wishes.service.ts
+++ src/app/services/wishes.service.ts
@@ -10,21 +10,36 @@ export class WishesService {
 
   constructor() {
 
+    this.loadLocalStg();
     // console.log('Service initialized');
 
-    const list1 = new List('Watering');
-    const list2 = new List('Propagation');
-    const list3 = new List('New projects');
-    const list4 = new List('Wiring');
+    // const list1 = new List('Watering');
+    // const list2 = new List('Propagation');
+    // const list3 = new List('New projects');
+    // const list4 = new List('Wiring');
 
-    this.lists.push(list1, list2, list3, list4);
+    // this.lists.push(list1, list2, list3, list4);
 
-    console.log(this.lists);
-
+    // console.log(this.lists);
   }
 
   createList( title: string) {
     const newList = new List(title);
     this.lists.push(newList);
+    this.saveLocalStg();
   }
+
+  saveLocalStg() {
+    localStorage.setItem('data', JSON.stringify(this.lists));
+  }
+
+  loadLocalStg() {
+    if(localStorage.getItem('data')){
+      this.lists = JSON.parse(localStorage.getItem('data'));
+      console.log(JSON.parse(localStorage.getItem('data')));
+    } else {
+      this.lists = [];
+    }
+  }
+  
 }
